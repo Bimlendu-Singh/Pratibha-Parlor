2c4d109f0288d033abc3a9b4c7818a6de24235ba
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:16:49 +0530

    footer.blade.php code added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- resources/views/layouts/footer.blade.php -->
+<footer>
+    <div class="footer-content">
+        <p>&copy; 2024 Pratibha Parlor. All Rights Reserved.</p>
+    </div>
+</footer>
