c6102001fd54e5177e6d1242d331495756f36d4e
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:15:47 +0530

    app.blade.php code added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratibha Parlor</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/animations.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/animations.js') }}" defer></script>
+</head>
+<body>
+    @include('layouts.header')
+
+    <div class="content">
+        @yield('content')
+    </div>
+
+    @include('layouts.footer')
+</body>
+</html>
