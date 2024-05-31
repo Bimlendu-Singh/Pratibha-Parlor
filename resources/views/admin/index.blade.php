88f4379007443bbaaed79a1f959cffa1755becfd
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:25:12 +0530

    admin index.blade.php code added

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/admin/index.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container fade-in">
+    <h1>Admin Dashboard</h1>
+    <p>Welcome to the admin dashboard.</p>
+</div>
+@endsection
