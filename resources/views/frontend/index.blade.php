11b73f4304de120525b1a0447bb2c0ea2936c20d
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:23:05 +0530

    index.blade.php code added

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/frontend/index.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container fade-in">
+    <h1>Welcome to Pratibha Parlor</h1>
+    <p>Experience the best services and ambiance.</p>
+    <button>Book Now</button>
+</div>
+@endsection
