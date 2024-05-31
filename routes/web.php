38166b04c44f080c8e2607d8ac29aa473f4a1825
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:07:20 +0530

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+// routes/web.php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+// Future Admin Route
+Route::prefix('admin')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
 });
