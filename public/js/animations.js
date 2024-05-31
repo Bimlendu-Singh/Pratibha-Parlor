7715fb036648d5d51009a98464ab185f42ad80d9
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-05-31 12:21:16 +0530

    animations.css code added

---
+++ public/js/animations.js
@@ -0,0 +1,7 @@
+// public/js/animations.js
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(element => {
+        element.classList.add('fade-in');
+    });
+});
